f9b83a53f92c104728bd2bc48c244b1f550ffa0d
David <33669220+DavidKaczmarek92@users.noreply.github.com>
2026-06-04 23:23:21 +0200

    fix: flag icons render incorrectly on windows (#18)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
 import App from "./App";
 import "./styles/index.css";
+
+polyfillCountryFlagEmojis("Twemoji Country Flags", "/fonts/TwemojiCountryFlags.woff2");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
