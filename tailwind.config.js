4b6f0b3d8344cafab14576d0faacef4f89339e3a
David <d.kaczmarek29@gmail.com>
2026-06-04 23:22:57 +0200

    fix: flag icons render incorrectly on windows

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Twemoji Country Flags"', 'Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
